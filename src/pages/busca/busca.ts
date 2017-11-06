9d360819910c57eae7b8898b242ccae1dfc14797
igormeireles <meireles.igor@yahoo.com.br>
2017-11-06 14:27:24 -0200

    adicionando tela de busca

--- src/pages/busca/busca.ts
+++ src/pages/busca/busca.ts
@@ -7,20 +7,5 @@ import { UserModel } from '../profile/profile.model';
   templateUrl: 'busca.html'
 })
 export class PagBusca {
-  list: Array<UserModel> = [];
 
-  constructor(public menu: MenuController, public navParams: NavParams)
-  {
-    this.list = navParams.get('list');
-  }
-
-  ionViewDidEnter() {
-    // the root left menu should be disabled on this page
-    this.menu.enable(false);
-  }
-
-  ionViewWillLeave() {
-    // enable the root left menu when leaving the tutorial page
-    this.menu.enable(true);
-  }
 }
